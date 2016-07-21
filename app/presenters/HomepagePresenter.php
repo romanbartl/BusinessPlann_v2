b4831d0b2636aa42b6fe7aa1e8f9a4562bc8a76e
Roman Bártl <bbartl.roman@gmail.com>
2016-07-21 20:50:04 +0200

    Přípravy pro registraci a přihlašování

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,10 +8,9 @@ use App\Model;
 
 class HomepagePresenter extends BasePresenter
 {
-
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
+    public function actionRedirect()
+    {
+        $this->redirect('Homepage:default', array('locale' => 'cs'));
+    }
 
 }
